ff4d3ce05ddbcaace94ace56ce94df254929201a
yami <88222513+29yami@users.noreply.github.com>
2024-10-24 20:50:21 -0500

    Settings change.

--- Settings.asm
+++ Settings.asm
@@ -6,9 +6,9 @@
 ZoneCount:				= 1	; discrete zones are: DEZ
 GameDebug:				= 1	; if 1, enable debug mode for Sonic
 GameDebugAlt:			= 0	; if 1, enable alt debug mode for Sonic
-Lagometer:				= 1	; if 1, enable debug lagometer
-BossDebug:				= 0	; if 1, one hit for all bosses
-ExtendedCamera:			= 0	; if 1, enable extended camera
+Lagometer:				= 0	; if 1, enable debug lagometer
+BossDebug:				= 1	; if 1, one hit for all bosses
+ExtendedCamera:			= 1	; if 1, enable extended camera
 RollInAir:				= 1	; if 1, enable roll in air for Sonic
 MSUMode:				= 0	; if 1, enable MSU
 OptimiseStopZ80:			= 2	; if 1, remove stopZ80 and startZ80, if 2, use only for controllers(ignores sound driver)
